e242fd904c17354c0e10cb404bb65a520a6aa507
eceberker <eceberker41@gmail.com>
2021-03-03 14:01:25 +0300

    redis up

--- db/db.go
+++ db/db.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -11,6 +12,7 @@ import (
 // ConnectDb creates database connection
 func ConnectDb() *sql.DB {
 
+	fmt.Println("Initializing database connection . . .")
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
@@ -19,23 +21,38 @@ func ConnectDb() *sql.DB {
 	if err != nil {
 		log.Fatal("Unable to read .env file")
 	}
-	dbname := envs["DB_NAME"]
-	dbsource := envs["DB_SOURCE"]
+	dbDriver := envs["POSTGRES_DRIVER"]
+	dbName := envs["POSTGRES_DB"]
+	dbUser := envs["POSTGRES_USER"]
+	dbPass := envs["POSTGRES_PASSWORD"]
+	dbHost := envs["POSTGRES_HOST"]
 
-	// Open the connection
-	db, err := sql.Open(dbname, dbsource)
+	dsn := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", dbUser, dbPass, dbHost, dbName)
 
-	if err != nil {
-		panic(err)
+	var db *sql.DB
+	var er error
+
+	tries := 10
+	for tries > 0 {
+		// // Open the connection
+		db, er = sql.Open(dbDriver, dsn)
+		if err != nil {
+			fmt.Printf("Unable to Open DB: %s... Retrying\n", er.Error())
+			time.Sleep(time.Second * 2)
+			tries--
+		} else if er = db.Ping(); er != nil {
+			// check the connection
+			fmt.Printf("Unable to Ping DB: %s... Retrying\n", er.Error())
+			time.Sleep(time.Second * 2)
+		} else {
+			er = nil
+			break
+		}
 	}
-	// check the connection
-	err = db.Ping()
-
-	if err != nil {
-		panic(err)
+	if er != nil {
+		db = nil
+		return db
 	}
-
-	fmt.Println("Successfully connected to Db!")
-	// return the connection
+	fmt.Println("Connection to Database successful")
 	return db
 }
